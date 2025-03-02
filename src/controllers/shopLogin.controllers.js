7bd23c8da1aad60deb75e97dad6c00c300926b3c
upvisor <admin@upvisor.cl>
2025-03-02 00:44:01 -0300

    actualization general

--- src/controllers/shopLogin.controllers.js
+++ src/controllers/shopLogin.controllers.js
@@ -3,16 +3,16 @@ import bcrypt from 'bcryptjs'
 
 export const createAccount = async (req, res) => {
     try {
-        const { name, email, password, type, permissions } = req.body
+        const { name, email, password, type, permissions, plan } = req.body
         const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
         if (!regex.test(email)) return res.send({ message: 'El email no es valido' })
         if (password.length < 6) return res.send({ message: 'La contraseña tiene que tener minimo 6 caracteres' })
         const user = await ShopLogin.findOne({ email: email })
         if (user) return res.send({ message: 'El email ya esta registrado' })
         const hashedPassword = await bcrypt.hash(password, 12)
-        const newAccount = new ShopLogin({ name, email, password: hashedPassword, type, permissions })
+        const newAccount = new ShopLogin({ name, email, password: hashedPassword, type, permissions, plan })
         const accountSave = await newAccount.save()
-        return res.send({ name, email: accountSave.email, _id: accountSave._id, type, permissions })
+        return res.send({ name, email: accountSave.email, _id: accountSave._id, type, permissions, plan })
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
